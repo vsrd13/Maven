5991bb9ef1f82fcc1b81131b934b705fac21e54b
vsrkd13 <vsrkd13@gmail.com>
2017-02-03 18:31:46 -0600

    Commiting the porject for Continuos Integration with Maven and Jenkins

---
+++ src/test/java/SampleMaven/MavenTest/SampleTest.java
@@ -0,0 +1,24 @@
+package SampleMaven.MavenTest;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@BeforeClass
+	public void setup() {
+		System.out.println("This is the start of the class..!!");
+	}
+
+	@AfterClass
+	public void close() {
+		System.out.println("This is the end of the class..!!");
+	}
+
+	@Test
+	public void testRun() {
+		System.out.println("Running the tests..!!");
+	}
+
+}
